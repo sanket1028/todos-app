9d15472b370da293078db1e0009cbbbea39bb7f8
sanket1028 <sanket.bulkunde7@gmail.com>
2025-04-03 00:34:09 +0530

    Login functionality added

--- controllers/User.js
+++ controllers/User.js
@@ -1,25 +1,28 @@
 const UserModel = require("../models/User");
 const { z } = require("zod");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+function authRequestValidation(req) {
+  const requiredAuthBody = z
+    .object({
+      email: z.string().min(5).max(30).email(),
+      password: z.string().min(8).max(20),
+    })
+    .strict();
+
+  return requiredAuthBody.safeParse(req.body);
+}
 
 exports.signup = async (req, res) => {
   try {
-    const requiredBody = z
-      .object({
-        email: z.string().min(5).max(30).email(),
-        password: z.string().min(8).max(20),
-      })
-      .strict();
-
-    const safeParseBody = requiredBody.safeParse(req.body);
-
+    const safeParseBody = authRequestValidation(req);
     if (!safeParseBody.success) {
       return res.status(400).json({
         message: safeParseBody.error.flatten(),
       });
     }
-
-    const { email, password } = safeParseBody.data;
+    const { email, password } = req.body;
 
     const hashedPassword = await bcrypt.hash(password, 5);
 
@@ -36,7 +39,45 @@ exports.signup = async (req, res) => {
     }
 
     return res.status(500).json({
-      message: "Internal Serever error",
+      message: "Internal Server error",
+    });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const safeParseBody = authRequestValidation(req);
+    if (!safeParseBody.success) {
+      return res.status(400).json({
+        message: safeParseBody.error.flatten(),
+      });
+    }
+
+    const user = await UserModel.findOne({ email: safeParseBody.data.email });
+    if (!user) {
+      return res.status(404).json({
+        message: `User does't exists`,
+      });
+    }
+    const isPasswordValid = await bcrypt.compare(
+      safeParseBody.data.password,
+      user.password
+    );
+
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        message: "Incorrect Password",
+      });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '24h'
+    });
+    return res.status(200).json({
+      token,
+    });
+  } catch (e) {
+    return res.status(500).json({
+      message: "Internal Server error",
     });
   }
 };
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { signup } = require('../controllers/User');
+const { signup, login} = require('../controllers/User');
 
 
 const router = express.Router();
 
-router.post('/signup', signup)
+router.post('/signup', signup);
+router.post('/login', login);
 
 module.exports = router
